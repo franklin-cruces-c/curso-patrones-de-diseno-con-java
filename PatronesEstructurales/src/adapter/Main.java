ec50dbdcc180c35b2ddbf64c38e64f24195272d5
franklin-cruces <franklin.cruces@hotmail.com>
2024-10-23 23:49:16 -0300

    Patrones Estructurales - Adapter. Se crea clase AdaptadorDB que implementa la interfaz IConexionSQL llamando los metodos de una implementacion de la Interfaz IConexionNoSQl adaptandola para que sea compatible y así realizandose una modificación minima en el cliente (Main) donde solo se modifica la creacion del objeto inicial

--- PatronesEstructurales/src/adapter/Main.java
+++ PatronesEstructurales/src/adapter/Main.java
@@ -3,7 +3,7 @@ package adapter;
 public class Main {
 
 	public static void main(String[] args) {
-		IConexionSQL conexion = new ConexionMySQL();
+		IConexionSQL conexion = new AdaptadorDB(new ConexionMongoDB());
 		/**
 		 * Si se necesitase cambiar esta conexion a Mongo DB el cliente (Main) falla ya
 		 * que no está preparado, tiene metodos que son diferentes como excecuteSentence
