889301bdd10854098bb551c255571f43ab897ce7
franklin-cruces <franklin.cruces@hotmail.com>
2024-10-25 13:16:44 -0300

    Patrones de Comportamiento - Mediator. Se implementa este patron tomando como ejemplo una sala de chat implementandolo en el metodo enviarMensaje de la clase SalaChat comunicando los objetos remitente y receptor de la clase Usuario

---
+++ PatronesDeComportamiento/src/mediator/Main.java
@@ -0,0 +1,17 @@
+package mediator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Usuario eduardo = new Usuario("Eduardo");
+		Usuario codi = new Usuario("Codi");
+
+		SalaChat sala = new SalaChat();
+
+		sala.addParticipante(eduardo);
+		sala.addParticipante(codi);
+
+		sala.enviarMensaje(eduardo, codi, "Hola desde el curso de patrones de diseño!");
+	}
+
+}
